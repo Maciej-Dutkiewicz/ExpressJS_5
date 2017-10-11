fd98896390c1d42db63bb69fad13cf2e9a706720
Maciek <m.p.dutkiewicz@gmail.com>
2017-10-11 20:12:49 +0200

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '1047196303937-v5r76hds1ob3ltdjhco9denvauhh34kk.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'd0vgc7yunYsRUqr8BUbxM06N',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
